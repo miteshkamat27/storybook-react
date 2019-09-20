525baabba18327634b192947bdf1f4ed5c11d8ac
Mitesh Kamat <mitesh.kamat@ril.com>
2019-09-20 16:38:56 +0530

    New deployment

---
+++ storybook-static/precache-manifest.fd2b368d34cf551a4013658e6df515f3.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bb2ba329e677e8a17672825619f2a6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.645ce6547d153e96d519.bundle.js"
+  },
+  {
+    "revision": "a5a367e67e6703ad817d",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.645ce6547d153e96d519.bundle.js"
+  },
+  {
+    "url": "vendors~main.645ce6547d153e96d519.bundle.js"
+  }
+]);
